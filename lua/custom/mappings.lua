510d540e71b936e3cb69391204cced51a0480988
Lewis Fidlers <lewisfidlers@gmail.com>
2025-07-02 11:25:04 +0200

    custom mappings

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_keymap('n', ':W', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ':WQ', ':wq<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ':Wq', ':wq<CR>', { noremap = true, silent = true })
