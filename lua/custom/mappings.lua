aee9f499d8a42792767d9f6d38a9f7084fb8fd0e
Lewis Fidlers <lewis.fidlers@du.co>
2025-08-14 09:31:32 +0200

    Vsplit go to def and avante plugin

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -6,3 +6,8 @@ vim.api.nvim_set_keymap('n', ':WQ', ':wq<CR>', { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', ':Wq', ':wq<CR>', { noremap = true, silent = true })
 
 vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
+
+vim.keymap.set('n', 'grv', function()
+  vim.cmd 'vsplit'
+  vim.lsp.buf.definition()
+end, { desc = 'Go to definition in vsplit' })
